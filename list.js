34239fc61a9a8aeaf57c4defbf9038b9e848b301
eduardoschulz <eduardo.schulz@protonmail.com>
2020-10-19 09:20:21 -0300

    add a method to remove tasks

--- list.js
+++ list.js
@@ -1,6 +1,9 @@
 tasks = [];
 //Cria uma Array para armazenar as tasks
 //pode ser util para armazenar em um arquivo
+var numTask = 1;
+//Define qual eh o id da task
+
 
 //quando o bt eh pressionado o valor eh guardado na array
 addEventListener("keyup", function(event) {
@@ -20,23 +23,29 @@ function btPress(){
     }
 
     else{
+        numTask += 1;
         console.log(inputTag);
         tasks.push(inputTag);
         console.log(tasks)
-   
-
-
-
+        
         var ul = document.getElementById("list");
         var li = document.createElement("li");
         li.appendChild(document.createTextNode(inputTag));
         ul.appendChild(li);
-
+        li.id  = numTask;   
     }
-    
+}
+function removeTask(){
+    //var remIdTask = document.getElementById.value('taskToRem');
+     // aqui se define o id da task que deseja-se deletar
+    //console.log(rem);
+    //rem.id.remove(remIdTask);//faz o contrario da funcao appendChild
+    var remIdTask = document.getElementById("taskToRem").value;
+    remIdTask.remove();
 
 
 }
+
 //https://github.com/electron-userland/electron-json-storage
 //Pode ser interessante
     
